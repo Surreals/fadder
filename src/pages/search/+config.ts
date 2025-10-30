c6a0415f9f878580af5a28c36cd8166113b8ca97
Marian Fedkovych <m.fed@intobi.com>
2025-10-30 18:57:14 +0200

    prerender: false

--- src/pages/search/+config.ts
+++ src/pages/search/+config.ts
@@ -3,4 +3,5 @@ import type { Config } from "vike/types";
 export default {
   title: "Search Results - FADDER",
   description: "Search results for vehicles",
+  prerender: false,
 } satisfies Config;
